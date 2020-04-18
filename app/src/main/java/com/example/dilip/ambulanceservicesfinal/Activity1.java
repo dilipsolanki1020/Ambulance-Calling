d25bae4a64a43f07caac21c81c741870bf048529
dilipsolanki1020 <48093771+dilipsolanki1020@users.noreply.github.com>
2020-04-18 15:43:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dilip/ambulanceservicesfinal/Activity1.java
@@ -0,0 +1,14 @@
+package com.example.dilip.ambulanceservicesfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+
+    }
+}
